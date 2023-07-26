b36bf23589f8603f30a7b995ee579b46a2caeeb6
seungji <dkstmdwl0615@naver.com>
2023-07-26 20:20:08 +0900

    style : 로그인 페이지 스타일 작업중, 비율, 컬러 및 폰트 등 공통 스타일 적용 필요 #37

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -40,42 +40,93 @@ function Login() {
   };
 
   return (
-    <div>
+    <div className="w-full max-w-xs mx-auto mt-20">
       <Tabs>
-        <TabList>
-          <Tab>User Login</Tab>
-          <Tab>Admin Login</Tab>
+        <TabList className="flex w-2/4 mb-3">
+          <Tab className="flex-1 py-2 text-sm text-center border-b-4 cursor-pointer focus:outline-none focus:shadow-outline focus:text-blue-500 focus:border-blue-500">
+            User Login
+          </Tab>
+          <Tab className="flex-1 py-2 text-sm text-center border-b-4 cursor-pointer focus:outline-none focus:shadow-outline focus:text-blue-500 focus:border-blue-500">
+            Admin Login
+          </Tab>
         </TabList>
 
         <TabPanel>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username">
-              Username:
-              <input type="text" name="username" onChange={handleInputChange} />
-            </label>
-            <label htmlFor="password">
-              Password:
-              <input type="password" name="password" onChange={handleInputChange} />
-            </label>
-            <input type="submit" value="Submit" />
+          <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
+            <div className="mb-4">
+              <label htmlFor="username" className="block mb-2 text-sm font-bold text-gray-700">
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+              </label>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block mb-2 text-sm font-bold text-gray-700">
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+              </label>
+            </div>
+            <div className="flex items-center justify-between">
+              <input
+                type="submit"
+                value="Submit"
+                className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              />
+            </div>
           </form>
         </TabPanel>
 
         <TabPanel>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username">
-              Username:
-              <input type="text" name="username" onChange={handleInputChange} />
-            </label>
-            <label htmlFor="password">
-              Password:
-              <input type="password" name="password" onChange={handleInputChange} />
-            </label>
-            <label htmlFor="centerCode">
-              Center Code:
-              <input type="text" name="centerCode" onChange={handleInputChange} />
-            </label>
-            <input type="submit" value="Submit" />
+          <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
+            <div className="mb-4">
+              <label htmlFor="username" className="block mb-2 text-sm font-bold text-gray-700">
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+              </label>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block mb-2 text-sm font-bold text-gray-700">
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+              </label>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="centerCode" className="block mb-2 text-sm font-bold text-gray-700">
+                Center Code:
+                <input
+                  type="text"
+                  name="centerCode"
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+              </label>
+            </div>
+            <div className="flex items-center justify-between">
+              <input
+                type="submit"
+                value="Submit"
+                className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              />
+            </div>
           </form>
         </TabPanel>
       </Tabs>

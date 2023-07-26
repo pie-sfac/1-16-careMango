beb0350a730d7379fb1956cad467efea4c35cb4c
lyeejj <lyshine012@gmail.com>
2023-07-26 18:16:43 +0900

    Design: tailwind 커스텀 스타일 생성 및 적용

--- src/routes/checkSchedule.tsx
+++ src/routes/checkSchedule.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Card from '../components/common/Card';
 
 const CheckSchedule = () => {
   const [itemData, setItemData] = useState();
@@ -16,7 +17,7 @@ const CheckSchedule = () => {
   if (!itemData) return <p>loading...</p>;
   return (
     <>
-      <header className="flex justify-between px-4 py-3">
+      <header className="flex justify-between py-3">
         <div className="flex">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
             <path
@@ -24,26 +25,26 @@ const CheckSchedule = () => {
               fill="#505050"
             />
           </svg>
-          <p>11시 서태지</p>
+          <p className="text-base">11시 서태지</p>
         </div>
         <div>
-          <button className="pl-5" type="button">
+          <button className="pl-5 text-base" type="button">
             변경
           </button>
-          <button className="pl-5" type="button">
+          <button className="pl-5 text-base" type="button">
             취소
           </button>
         </div>
       </header>
-      <section className="bg-[#F4F4F4] w-full h-40 relative">
-        <div className="flex items-center justify-between w-11/12 py-4 m-auto">
-          <h1 className="text-base font-bold">개인수업일정</h1>
-          <p className="text-[#AEAEAE] text-xs">생성일 2022년 12월 09일 (금) 13시 30분 김파이</p>
+      <section className="relative h-40 bg-bg-100">
+        <div className="flex items-center justify-between py-4">
+          <h1 className="main-title">개인수업일정</h1>
+          <p className="text-xs text-text-400">생성일 2022년 12월 09일 (금) 13시 30분 김파이</p>
         </div>
 
-        <div className="absolute left-0 w-full top-24">
-          <h2 className="w-11/12 py-2 m-auto text-sm font-bold">수업 정보</h2>
-          <div className="text-sm m-auto w-11/12 h-16 p-6 bg-white border border-[#E7E7E7] rounded-xl flex justify-start items-center gap-7">
+        <div className="absolute left-0 w-11/12 top-24">
+          <h2 className="small-title">수업 정보</h2>
+          <div className="flex items-center justify-start h-16 p-6 gap-7 card-border">
             <div className="flex gap-4">
               <p className="font-bold">일정</p>
               <p>2022.12.23 (금)</p>
@@ -64,11 +65,11 @@ const CheckSchedule = () => {
         </div>
       </section>
 
-      <section className="w-11/12 m-auto mt-20">
-        <h2 className="my-2 text-sm font-bold">참여회원(1)</h2>
-        <div className="flex items-center text-sm">
-          <div className="border border-line-200 rounded-xl">
-            <div className="flex items-center gap-28 p-5 border-b border-[#E7E7E7]">
+      <section className="mt-20">
+        <h2 className="small-title">참여회원(1)</h2>
+        <div className="flex items-center">
+          <Card>
+            <div className="flex items-center p-5 border-b gap-28 border-line-200">
               <div className="flex gap-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <g clipPath="url(#clip0_18_8567)">
@@ -88,15 +89,15 @@ const CheckSchedule = () => {
                 </svg>
 
                 <div className="flex flex-col">
-                  <p className="font-bold">서태지황황</p>
-                  <p>(010-0000-0000)</p>
+                  <p className="font-bold">{itemData.attendanceHistories[0].member.name}</p>
+                  <p>{itemData.attendanceHistories[0].member.phone}</p>
                 </div>
               </div>
               <div className="flex gap-1">
-                <button type="button" className="px-5 py-2 border border-[#E7E7E7] rounded">
+                <button type="button" className="attendance-btn">
                   출석
                 </button>
-                <button type="button" className="px-5 py-2 border border-[#E7E7E7] rounded">
+                <button type="button" className="attendance-btn">
                   결석
                 </button>
               </div>
@@ -111,12 +112,11 @@ const CheckSchedule = () => {
               </ul>
               <ul className="flex flex-col gap-2 font-bold">
                 <li>{itemData.attendanceHistories[0].status === 'WAIT' ? '예약' : '출석/결석'}</li>
-                <li>개인 PT 수강권(50분)</li>
+                <li>{itemData.issuedTicket.title}</li>
                 <li>
                   {itemData.issuedTicket.remainingCount}회 (총 {itemData.issuedTicket.defaultCount}회)
                 </li>
                 <li>
-                  {' '}
                   {itemData.issuedTicket.availableReservationCount}회 (총 {itemData.issuedTicket.defaultCount}회)
                 </li>
               </ul>
@@ -124,16 +124,16 @@ const CheckSchedule = () => {
             <div className="mb-6 px-14">
               <button
                 type="button"
-                className="mr-2 text-xs px-3 py-2 text-[#6691FF] inline-flex border border-[#E7E7E7] rounded-xl">
+                className="inline-flex px-3 py-2 mr-2 text-xs border text-primary-300 border-line-200 rounded-xl">
                 기록 작성하기
               </button>
               <button
                 type="button"
-                className="text-xs px-3 py-2 text-[#6691FF] inline-flex border border-[#E7E7E7] rounded-xl">
+                className="inline-flex px-3 py-2 text-xs border text-primary-300 border-line-200 rounded-xl">
                 퍼스널 레포트 보내기
               </button>
             </div>
-          </div>
+          </Card>
         </div>
       </section>
     </>

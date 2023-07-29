3fd99c23aa88bc8155ddc8a313143ad212915cd1
seungji <dkstmdwl0615@naver.com>
2023-07-29 16:57:52 +0900

    feat : 메인 페이지 마크업 진행중

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,65 +1,101 @@
-import React, { useEffect, useState } from 'react';
-
-interface Member {
-  id: number;
-  name: string;
-  phone: string;
-  sex: string;
-  birthDate: string;
-  createdAt: string;
-  updatedAt: string;
-  visitedAt: string;
-}
-
-const memberDatas: Member[] = [
-  {
-    id: 1,
-    name: '김회원',
-    phone: '01012341234',
-    sex: 'MALE',
-    birthDate: '2023-07-28',
-    createdAt: '2023-07-28T07:38:08.832Z',
-    updatedAt: '2023-07-28T07:38:08.832Z',
-    visitedAt: '2023-07-28T07:38:08.832Z',
-  },
-  {
-    id: 2,
-    name: '박회원',
-    phone: '01012341234',
-    sex: 'FEMALE',
-    birthDate: '2023-07-28',
-    createdAt: '2023-07-28T07:38:08.832Z',
-    updatedAt: '2023-07-28T07:38:08.832Z',
-    visitedAt: '2023-07-28T07:38:08.832Z',
-  },
-];
+import React from 'react';
 
 function Main() {
-  const [memberList, setMemberList] = useState<Member[] | null>(null);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setMemberList(memberDatas);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
-
+  const planStatus: string = '플랜 이용중';
+  const alarm: string = '알람 없음';
   return (
-    <div>
-      {memberList &&
-        memberList.map((member, index) => (
-          <div key={member.id}>
-            <p>ID: {member.id}</p>
-            <p>Name: {member.name}</p>
-            <p>Phone: {member.phone}</p>
-            <p>Sex: {member.sex}</p>
-            <p>Birth Date: {member.birthDate}</p>
-            <p>Created At: {member.createdAt}</p>
-            <p>Updated At: {member.updatedAt}</p>
-            <p>Visited At: {member.visitedAt}</p>
-          </div>
-        ))}
-    </div>
+    <>
+      <header className="flex">
+        <h1>poinT</h1>
+        <div className="flex">
+          <p>박관리자</p>
+          <div>{planStatus}</div>
+          <div>{alarm}</div>
+        </div>
+      </header>
+      <div>
+        <label htmlFor="search">
+          <input type="text" name="search" placeholder="회원/멤버 이름, 연락처로 검색하세요" />
+          <input type="submit" value="Submit" className="cursor-pointer" />
+        </label>
+      </div>
+      <div>
+        <ul className="flex">
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">광고1</a>
+          </li>
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">광고2</a>
+          </li>
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">광고3</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="flex">
+          <li>
+            <span>나의 오늘 일정</span>
+            <div className="card_wrapper">
+              <div className="card">
+                <div className="card_upper">
+                  <span>총 8건의 일정</span>
+                  <span>수업 7건, 상담 1건</span>
+                </div>
+                <div className="card_down">
+                  <span>8</span>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <span>나의 회원</span>
+            <div className="card_wrapper">
+              <div className="card">
+                <div className="card_upper">
+                  <span>나의 회원 수</span>
+                </div>
+                <div className="card_down">
+                  <span>16</span>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <span>전체 직원</span>
+            <div className="card_wrapper">
+              <div className="card">
+                <div className="card_upper">
+                  <span>전체 직원 수</span>
+                </div>
+                <div className="card_down">
+                  <span>80</span>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom_wrapper">
+        <ul className="flex bottom_bar">
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">홈</a>
+          </li>
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">일정관리</a>
+          </li>
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">회원관리</a>
+          </li>
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">센터관리</a>
+          </li>
+          <li>
+            <a href="https://github.com/pie-sfac/1-16-careMango">마이페이지</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 

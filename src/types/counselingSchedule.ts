65d66f4c9cd4233a7c4325363a4359fa4bd0e2a5
yueunkim <yueunkim0605@gmail.com>
2023-07-29 22:39:45 +0900

    style: eslint 규칙에 맞게 수정

--- src/types/counselingSchedule.ts
+++ src/types/counselingSchedule.ts
@@ -1,41 +1,41 @@
 export interface CounselingScheduleItemData {
-  id: number,
-  startAt: string,
-  endAt: string,
-  memo: string,
-  isCanceled: true,
-  canceledAt: string,
+  id: number;
+  startAt: string;
+  endAt: string;
+  memo: string;
+  isCanceled: true;
+  canceledAt: string;
   counselor: {
-    id: 0,
-    name: string
-  },
+    id: 0;
+    name: string;
+  };
   client: {
-    memberId: 0,
-    name: string,
-    phone: string
-  },
+    memberId: 0;
+    name: string;
+    phone: string;
+  };
   counselingRecord: {
-    id: 0,
-    content: string,
+    id: 0;
+    content: string;
     createdBy: {
-      id: 0,
-      name: string
-    },
+      id: 0;
+      name: string;
+    };
     updatedBy: {
-      id: 0,
-      name: string
-    },
-    createdAt: string,
-    updatedAt: string
-  },
-  createdAt: string,
+      id: 0;
+      name: string;
+    };
+    createdAt: string;
+    updatedAt: string;
+  };
+  createdAt: string;
   createdBy: {
-    id: 0,
-    name: string
-  },
-  updatedAt: string,
+    id: 0;
+    name: string;
+  };
+  updatedAt: string;
   updatedBy: {
-    id: 0,
-    name: string
-  }
+    id: 0;
+    name: string;
+  };
 }

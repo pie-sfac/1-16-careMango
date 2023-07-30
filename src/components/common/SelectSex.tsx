c8ac6af76600ac050d975676303dbfd90bfbea0a
yueunkim <yueunkim0605@gmail.com>
2023-07-30 17:34:10 +0900

    Feat: 생년월일, 성별, 직업, 방문경로 선택 컴포넌트 생성

---
+++ src/components/common/SelectSex.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+interface SelectSexProps {
+  title: string;
+  defaultState?: string;
+  onSelect?: (selectedSex: string) => void;
+}
+
+const SelectSex = ({ title, defaultState, onSelect }: SelectSexProps) => {
+  const [state, setState] = useState<string>(defaultState || '');
+
+  const handleClick = (sex: string) => {
+    setState(sex);
+    onSelect?.(sex);
+  };
+
+  return (
+    <div>
+      <p className="small-title">
+        {title}
+        <span className="text-primary-300">*</span>
+      </p>
+      <button
+        type="button"
+        className={`mr-1 attendance-btn ${state === '여' ? 'bg-primary-300 text-white' : ''}`}
+        onClick={() => handleClick('여')}>
+        여
+      </button>
+      <button
+        type="button"
+        className={`attendance-btn ${state === '남' ? 'bg-primary-300 text-white' : ''}`}
+        onClick={() => handleClick('남')}>
+        남
+      </button>
+    </div>
+  );
+};
+
+export default SelectSex;

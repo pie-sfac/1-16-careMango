7b9ac823df510a0b9b3f7d96ec4cf7d8f7e9d797
yueunkim <yueunkim0605@gmail.com>
2023-08-01 22:55:15 +0900

    Feat: 회원 조회 페이지에서 클릭 시 api연동하여 해당 회원 정보 페이지로 연결

---
+++ src/types/members/membersDetail.ts
@@ -0,0 +1,14 @@
+export interface MembersDetail {
+  id: number;
+  name: string;
+  phone: string;
+  job: string;
+  birthDate: string;
+  sex: string;
+  acqusitionFunnel: string;
+  acquisitionFunnel: string;
+  visitedAt: string;
+  createdAt: string;
+  updatedAt: string;
+  message: string;
+}

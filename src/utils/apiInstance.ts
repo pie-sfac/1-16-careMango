51b464e9584253a58c2757c35c77279a0fd15a3b
seungji <dkstmdwl0615@naver.com>
2023-08-01 16:44:37 +0900

    fix : 토큰 갱신 기능 작동하지 않는 문제 해결 중...

--- src/utils/apiInstance.ts
+++ src/utils/apiInstance.ts
@@ -1,22 +1,6 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
 const useTokenRefresher = () => {
   useEffect(() => {
     const interval = setInterval(
@@ -48,5 +32,44 @@ const useTokenRefresher = () => {
     };
   }, []);
 };
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest.isRetry) {
+      originalRequest.isRetry = true;
+
+      try {
+        // Wait for token refresh to complete
+        await useTokenRefresher();
+
+        // Update Authorization header with new token
+        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+
+        // Retry the original request
+        return await axiosInstance(originalRequest);
+      } catch (err) {
+        console.error(err);
+        // handle error appropriately
+      }
+    }
+
+    // If error was not 401 or there is another issue, reject the promise
+    return Promise.reject(error);
+  },
+);
 
 export { axiosInstance, useTokenRefresher };

8ff5bb5f9588295890a0a53e780bb57bfb34bdac
lyeejj <lyshine012@gmail.com>
2023-08-03 01:19:20 +0900

    Feat: 날짜 단위에 따른 유효기간 설정함수 생성

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,6 @@
 // 날짜 생성함수
-export const getDay = (dateString: string, type?: string) => {
-  const date = new Date(dateString);
+export const getDay = (dateString?: string, type?: string) => {
+  const date = dateString ? new Date(dateString) : new Date();
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
@@ -19,4 +19,29 @@ export const getTime = (dateString: string) => {
   let minute: number | string = date.getMinutes();
   minute = minute < 10 ? `0${minute}` : minute;
   return `${hour}:${minute}`;
+};
+
+// 날짜 단위에 따라 계산하는 함수
+export const calculateDate = (date: string, unit: string, amount: number) => {
+  const calculatedDate = new Date(date);
+  const startDate = new Date(date);
+
+  switch (unit) {
+    case 'day':
+      calculatedDate.setDate(startDate.getDate() + amount);
+      break;
+    case 'week':
+      calculatedDate.setDate(startDate.getDate() + amount * 7);
+      break;
+    case 'month':
+      calculatedDate.setMonth(startDate.getMonth() + amount);
+      break;
+    case 'year':
+      calculatedDate.setFullYear(startDate.getFullYear() + amount);
+      break;
+    default:
+      break;
+  }
+
+  return calculatedDate;
 };

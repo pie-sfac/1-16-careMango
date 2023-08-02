c5fdccddfe941804f26b0c335ec054d7bcd2e7d3
lyeejj <lyshine012@gmail.com>
2023-08-03 02:03:25 +0900

    Feat: 수강권 부여 화면 구현

--- src/routes/ticket/ticketDetail.tsx
+++ src/routes/ticket/ticketDetail.tsx
@@ -3,9 +3,12 @@ import { useParams } from 'react-router-dom';
 import SubHeader from '../../components/common/SubHeader';
 import { axiosInstance } from '../../utils/apiInstance';
 import { TicketsData } from '../../types/tickets/tickets';
-import { LessonTypeEnum } from '../../enums/Ticket';
+import { LessonTypeEnum, TermUnitEnum } from '../../enums/Ticket';
 import Input from '../../components/common/Input';
 import { calculateDate, getDay, getDay2 } from '../../utils/date';
+import { ReactComponent as Plus } from '../../assets/icons/Plus.svg';
+import { ReactComponent as Minus } from '../../assets/icons/Minus.svg';
+import SelectCounselor from '../../components/common/SelectCounselor';
 
 const TicketDetail = () => {
   const [ticket, setTicket] = useState<TicketsData | null>(null);
@@ -13,6 +16,7 @@ const TicketDetail = () => {
 
   const [startDate, setStartDate] = useState(getDay());
   const [endDate, setEndDate] = useState('');
+  const [privateTutorId, setPrivateTutorId] = useState(0);
 
   const getTicket = useCallback(async () => {
     const res = await axiosInstance.get(`tickets/${ticketId}`);
@@ -46,7 +50,7 @@ const TicketDetail = () => {
       {ticket && (
         <>
           <div className="flex items-center gap-4 mt-10 mb-6">
-            <p className="my-2 font-bold">{ticket.title}</p>
+            <h1 className="main-title">{ticket.title}</h1>
             <p className="text-sm text-primary-500">{LessonTypeEnum[ticket.lessonType]}</p>
           </div>
 
@@ -57,8 +61,54 @@ const TicketDetail = () => {
           <label htmlFor="start" className="block mt-10 mb-2">
             유효기간 <span className="text-primary-300">*</span>
           </label>
-          <Input name="start" type="date" value={startDate} onChange={handleChange} />
-          <Input name="end" type="date" value={endDate} onChange={handleChange} />
+          <div className="flex items-center">
+            <Input name="start" type="date" value={startDate} onChange={handleChange} /> ~
+            <Input name="end" type="date" value={endDate} onChange={handleChange} />
+          </div>
+
+          <Input
+            name="defaultTerm"
+            type="text"
+            value={ticket.defaultTerm}
+            label="수강권기간"
+            width="w-80"
+            unit={TermUnitEnum[ticket.defaultTermUnit]}
+          />
+
+          <Input
+            name="defaultCount"
+            type="number"
+            value={ticket.defaultCount}
+            label="기본횟수"
+            unit="회"
+            width="w-80"
+          />
+
+          <Input
+            name="maxServiceCount"
+            type="number"
+            value={ticket.maxServiceCount}
+            label="서비스 횟수"
+            leftBtn={
+              <button type="button" className="mr-2 icon-btn">
+                <Minus />
+              </button>
+            }
+            rightBtn={
+              <button type="button" className="ml-2 icon-btn">
+                <Plus />
+              </button>
+            }
+            unit="회"
+            align="text-center"
+            width="w-72"
+          />
+
+          <SelectCounselor title="담당 강사 선택" onChange={(e) => setPrivateTutorId(e.target.value)} />
+
+          <button className="w-full py-3 my-5 text-white rounded mt-36 bg-primary-500" type="submit">
+            저장
+          </button>
         </>
       )}
     </>

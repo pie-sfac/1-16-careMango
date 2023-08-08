d4d210fcff72b017a4cb6478e2e3542f86c03987
lyeejj <lyshine012@gmail.com>
2023-08-08 15:01:48 +0900

    Fix: Home 컴포넌트 경로 변경

--- src/routes/auth/LoginRouter.tsx
+++ src/routes/auth/LoginRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import Login from '../login';
 import { accessTokenState } from '../../atoms/token/accessTokenAtom';

9d0b8859defc310d3e0ecb4daa73a277c878e186
seungji <dkstmdwl0615@naver.com>
2023-08-15 14:59:53 +0900

    chore : import react 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import ScheduleDetailPage from '@pages/schedule/getScheduleDetail';
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { axiosInstance } from '@/utils/apiInstance';
 import { ReactComponent as BlankPerson } from '@/assets/icons/BlankPerson.svg';
 import SearchBox from '@components/common/SearchBox';

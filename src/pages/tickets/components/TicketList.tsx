5de45f1f52aeaa3e4a97cadd0e6d94641dd922d6
lyeejj <lyshine012@gmail.com>
2023-08-20 13:40:26 +0900

    Feat: 수강권리스트 Empty state 화면 구현

--- src/pages/tickets/components/TicketList.tsx
+++ src/pages/tickets/components/TicketList.tsx
@@ -2,10 +2,11 @@ import { useCallback, useEffect, useState } from 'react';
 import { axiosInstance } from '@/utils/apiInstance';
 import { TicketsData } from '@/types/tickets/tickets';
 import TicketItem from './TicketItem';
+import { ReactComponent as EmptyMembership } from '@/assets/icons/EmptyMembership.svg';
 
 type TabType = 'active' | 'deactive';
 
-const TicketList = ({ tab }: { tab: boolean }) => {
+const TicketList = ({ tab }: { tab?: boolean }) => {
   const [activeTicketList, setActiveTicketList] = useState<TicketsData[] | null>(null);
   const [deactiveTicketList, setDeactiveTicketList] = useState<TicketsData[] | null>(null);
   const [activeTab, setActiveTab] = useState<TabType>('active');
@@ -25,7 +26,17 @@ const TicketList = ({ tab }: { tab: boolean }) => {
 
   return (
     <>
-      {tab && (
+      {!activeTicketList?.length && (
+        <div className="flex-col w-full mt-24 flex-center">
+          <EmptyMembership />
+          <p className="p-3 my-1 text-center base-font text-text-400">
+            센터에서 생성한 수강권이 없습니다.
+            <br />
+            수강권을 생성해 주세요.
+          </p>
+        </div>
+      )}
+      {tab && activeTicketList?.length && (
         <div className="flex w-56 mt-8 border-b tabs border-line-300 text-text-400">
           <button
             type="button"

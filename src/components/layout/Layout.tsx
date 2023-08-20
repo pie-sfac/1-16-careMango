6fe4b3c0e14c3d4ff6739d123a8d1b9018922472
lyeejj <lyshine012@gmail.com>
2023-08-20 13:15:05 +0900

    Fix: 메인헤더 메뉴, 레이아웃 변경

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import MainHeader from '@components/layout/MainHeader';
-import BottomNav from '@components/layout/BottomNav';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,10 +10,6 @@ const Layout = ({ children }: LayoutProps) => {
   const location = useLocation();
   const pathName = location.pathname;
 
-  let menu = false;
-  if (pathName.includes('/tickets') || pathName.includes('/staffs') || pathName.includes('/myPage')) {
-    menu = true;
-  }
   let bgColor = '';
   if (pathName === '/schedule' || pathName === '/members' || pathName === '/staffs' || pathName === '/myPage') {
     bgColor = 'bg-bg-100';
@@ -22,9 +17,8 @@ const Layout = ({ children }: LayoutProps) => {
 
   return (
     <>
-      {menu ? <MainHeader menu /> : <MainHeader />}
+      <MainHeader />
       <main className={`pt-12 pb-24 base-px ${bgColor} min-h-screen overflow-auto box-border`}>{children}</main>
-      <BottomNav />
     </>
   );
 };
